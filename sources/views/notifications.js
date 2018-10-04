9bbf430327f51272931b590e517b358f128d8f94
Nastya <akovalenko@xbsoftware.com>
2018-10-04 12:50:42 +0300

    [fix] notifications

--- sources/views/notifications.js
+++ sources/views/notifications.js
@@ -1,5 +1,5 @@
 import {JetView} from "webix-jet";
-// import {getNotifications} from "models/notifications";
+import {getNotifications} from "models/notifications";
 import {newNotification} from "models/newnotifications";
 
 export default class NotificationPopup extends JetView {
@@ -13,36 +13,38 @@ export default class NotificationPopup extends JetView {
 				borderless:true,
 				css:"notifications",
 				width:250,
-				autoheight:true,
+				height:385,
 				template:obj => {
 					return "<span class='m_title'>"+ _(obj.title) + "</span>" +
 						"<span class='message'>" + _(obj.message) + "</span>";
 				},
 				type:{
-					height:80
-				},
-				data:[
-					{ title:"Latest tasks in your projects", message:"Top margin in a popup window is smaller than the...", read:0 },
-					{ title:"Autocat report", message:"Navigations problem in the left menu", read:0 },
-					{ title:"Assignment", message:"Gita Noda assigned you a new ticket", read:1 },
-					{ title:"Lorena Rush added new tasks", message:"'Confirm' button styling", read:1 },
-					{ title:"Cubebeat report", message:"No ability to exit profile editing dialogue", read:1 }
-				]
+					height:"auto"
+				}
 			},
 			on:{
 				onHide:() => {
+					const list = this.$$("list");
+					list.clearAll();
+					list.showOverlay("<div style='margin:20px; font-size:14px;'>No new notifications</div>");
+					list.define({ height:80 });
+        			list.resize();
 					this.app.callEvent("read:notifications");
-					this.$$("list").clearAll();
 				}
 			}
 		};
 	}
 	init(){
 		const list = this.$$("list");
-		//list.sync(getNotifications());
+		list.parse(getNotifications());
+
+		webix.extend(list,webix.OverlayBox);
 
 		this.on(this.app,"new:notification",() => {
+			list.hideOverlay();
 			list.add(newNotification(),0);
+			list.define({ height:list.count()*104 });
+        	list.resize();
 		});
 	}
 	showWindow(pos){
