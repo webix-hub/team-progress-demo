a3a6536c5687f7838671a57f47c386ec711426cd
Maksim Kozhukh <mkozhukh@ya.ru>
2018-10-17 14:13:42 +0300

    [dev] auto-release to webix.com

---
+++ index.production.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Webix Team Progress App</title>
+	<!-- Webix Library -->
+	<script type="text/javascript" src="//cdn.webix.com/pro/edge/webix.js?v=6.0.0"></script>
+	<link rel="stylesheet" type="text/css" href="//cdn.webix.com/pro/edge/webix.css?v=6.0.0">
+
+	<!-- Material Design Icons -->
+	<link rel="stylesheet" type="text/css" href="//cdn.materialdesignicons.com/2.7.94/css/materialdesignicons.css">
+
+	<!-- App -->
+	<script type="text/javascript" src="codebase/myapp.js"></script>
+	<link rel="stylesheet" type="text/css" href="codebase/myapp.css">
+
+	<!-- NAVBAR -->
+	<script type="text/javascript" src="/demos/common/nav.js"></script>
+	<link rel="stylesheet" type="text/css" href="/demos/common/nav.css">
+
+</head>
+<body>
+	<script>
+		if (!webix.env.touch && webix.ui.scrollSize && webix.CustomScroll)
+			webix.CustomScroll.init();
+			
+		webix.protoUI({
+			name:"team-progress-demo",
+			app: webixjetapp.default
+		}, webix.ui.jetapp);
+
+		webix.ready(function(){
+			webix.ui({
+				rows:[
+					{ view:"team-progress-demo" },
+					{ view:"navbar", value:"team-progress" }
+				]
+			});
+		});
+	</script>
+</body>
+</html>
